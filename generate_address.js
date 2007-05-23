262f488299ab221c9a97fc82e39184113b5923ad
Niels Drost <n.drost@esciencecenter.nl>
2007-05-23 09:34:15 +0000

    obfuscated mailto links using javascript

--- generate_address.js
+++ generate_address.js
@@ -4,3 +4,10 @@ function generate_address( username, domain ) {
 	document.write("(<" + "a" + " " + "href=" + "mai" + "lto:" + addr + ">"
 			+ addr + "<\/a>)");
 } 
+
+function generate_contact_address( username, domain ) {
+	var atsign = "&#64;";
+	var addr = username + atsign + domain;
+	document.write("<" + "a" + " class=topmenu " + "href=" + "mai" + "lto:" + addr + ">"
+			+ "Contact" + "<\/a>");
+}

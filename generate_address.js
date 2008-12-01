ac690efe36b85779f7918298ba95c961a5336c75
Niels Drost <n.drost@esciencecenter.nl>
2008-12-01 00:12:36 +0000

    website just about done :)

--- generate_address.js
+++ generate_address.js
@@ -5,16 +5,25 @@ function generate_address( username, domain ) {
 			+ addr + "<\/a>)");
 } 
 
+function generate_address_bare( username, domain ) {
+	var atsign = "&#64;";
+	var addr = username + atsign + domain;
+	document.write("<" + "a" + " " + "href=" + "mai" + "lto:" + addr + ">"
+			+ addr + "<\/a>");
+} 
+
+
+
 function generate_contact_address( username, domain ) {
 	var atsign = "&#64;";
 	var addr = username + atsign + domain;
-	document.write("<" + "a" + " class=topmenu " + "href=" + "mai" + "lto:" + addr + ">"
+	document.write("<" + "a " + "href=" + "mai" + "lto:" + addr + ">"
 			+ "Contact" + "<\/a>");
 }
 
 function generate_menu_address( username, domain ) {
 	var atsign = "&#64;";
 	var addr = username + atsign + domain;
-	document.write("<" + "a" + " class=button " + "href=" + "mai" + "lto:" + addr + ">"
+	document.write("<" + "a" + "href=" + "mai" + "lto:" + addr + ">"
 			+ "Contact" + "<\/a>");
 }
